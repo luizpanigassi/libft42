50a4fba147494516af568ac41e296c384c9c4820
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 18:16:42 -0300

    Update ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -9,9 +9,13 @@
 /*   Updated: 2024/10/15 17:36:36 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+// This function writes a single character c to the file descriptor fd.
+// It uses the write system call to output the character. The function
+// takes two parameters: the character to be written and the file 
+// descriptor, allowing for flexible output to different destinations 
+// (like standard output, files, etc.).
 
 #include <unistd.h>
-
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
