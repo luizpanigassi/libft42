c5f7ad4ad857f7cbc3d9f630a755d0d449af7835
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 16:08:51 -0300

    Update ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -9,7 +9,9 @@
 /*   Updated: 2024/10/09 18:28:54 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This simple function is used to check if the characters in a string are numbers.
+// We use a while loop to iterate through the string and check if the characters are numbers individually. 
+//If there is a single character that is not a number, it returns 0, and stops the iteration.
 int	ft_isdigit(char *str)
 {
 	if (*str == '\0')
