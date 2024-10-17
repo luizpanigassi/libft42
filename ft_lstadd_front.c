a8197bfafd37bd7e604e6d4b25f36151a11b6571
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 17:59:52 -0300

    Update ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -9,7 +9,12 @@
 /*   Updated: 2024/10/17 15:05:28 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This function adds a new node at the front of the linked list. It uses the 
+// t_list structure to represent the nodes. The function takes a pointer to 
+// the head pointer (t_list **lst) so it can update the original head of the 
+// list. The new node's next pointer is set to the current head, and then the 
+// head pointer is updated to point to the new node, making it the first node 
+// in the list.
 #include <stddef.h>
 
 typedef struct s_list
