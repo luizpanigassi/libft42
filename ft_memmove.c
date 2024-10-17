e9ab6a18d15a1d9091346e1123293c5e3e9a98f6
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 16:45:57 -0300

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -9,7 +9,12 @@
 /*   Updated: 2024/10/10 16:29:12 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//This function works almost like memcpy, it copies a block of memory to a new location.
+//However, this function will check if the new block of memory overlaps by comparing
+//d and s. If the location of d is higher than s, it means that d was allocated AFTER
+//s in the memory, so it copies the data from end to start, to prevent overlap.
+//If it's the other way around, it copies from start to end, since the destination was
+//allocated before the source.
 #include <stddef.h>
 
 void	*ft_memmove(void *dest, void *src, size_t len)
