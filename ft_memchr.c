7f2d71aa76bb11be4747936b1dc24b0760d66222
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 17:09:40 -0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -9,7 +9,10 @@
 /*   Updated: 2024/10/10 17:55:36 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This function searches for the first occurrence of the character 'c' 
+// in a block of memory pointed to by 'str', examining up to 'n' bytes. 
+// If the character is found, it returns a pointer to the first occurrence; 
+// if not, it returns NULL.
 #include <stddef.h>
 
 void	*ft_memchr(void *str, int c, size_t n)
