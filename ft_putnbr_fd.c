2db0d280c936f97084a93d7202de552b4de478ff
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 18:22:17 -0300

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -9,7 +9,13 @@
 /*   Updated: 2024/10/15 17:57:51 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This function outputs an integer n to the specified file descriptor fd.
+// It handles the special case of the minimum integer value (-2147483648) 
+// by directly printing its string representation. If n is negative, 
+// it first outputs a '-' character and then converts n to a positive 
+// value. The function recursively processes the number to extract and 
+// print each digit, starting from the most significant digit. Finally, 
+// it writes each digit to the specified file descriptor using ft_putchar_fd.
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
