ac4914eda53566ed2b3131a81491f67a1b13dbf9
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 18:31:17 -0300

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -9,7 +9,13 @@
 /*   Updated: 2024/10/15 17:19:21 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This function applies a given function f to each character of the string s, 
+// transforming it based on the index of the character. It allocates memory for 
+// a new string to hold the transformed characters. The function iterates over 
+// each character in s, applying f to each character along with its index, 
+// and stores the result in the new string. After processing all characters, 
+// it adds a null terminator and returns a pointer to the newly created string. 
+// If the input string is NULL or memory allocation fails, it returns NULL.
 #include <stdlib.h>
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
