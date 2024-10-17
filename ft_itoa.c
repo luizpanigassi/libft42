b4861122e304e0bdbe00c576f29d4b97b416aedf
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 18:14:09 -0300

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -9,7 +9,13 @@
 /*   Updated: 2024/10/15 17:05:38 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This function converts an integer (n) into a null-terminated string representation.
+// It handles negative numbers and the special case of zero. 
+// The function first calculates the length of the string needed using ft_num_length. 
+// It allocates memory for the string, including space for the null terminator. 
+// If the input number is negative, it places a '-' at the start of the string.
+// It fills the string with digits from the least significant to the most significant 
+// and returns a pointer to the resulting string. If memory allocation fails, it returns NULL.
 #include <stdlib.h>
 
 int	ft_num_length(int num)
