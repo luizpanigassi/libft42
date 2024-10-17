6f2d8f38c36f7fbc666b0bda4d4442897266ad21
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 16:15:15 -0300

    Update ft_toupper.c

--- ft_toupper.c
+++ ft_toupper.c
@@ -9,7 +9,9 @@
 /*   Updated: 2024/10/09 18:58:37 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//This simple function replaces lowercase characters for their uppercase counterparts.
+//We iterate through the string, checking if the character is lower/uppercase, and replace
+//it accordingly, then return the string in the end.
 char	*ft_toupper(char *str)
 {
 	char	*ptr;
