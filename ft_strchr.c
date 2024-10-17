f3a41a88c906ed4f72e1679f7cda5d4f3efcd7db
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 16:18:06 -0300

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -9,7 +9,10 @@
 /*   Updated: 2024/10/09 19:19:17 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//This function will return the first occurence of the character presented by c
+//in the string str. We iterate through the string using a while loop until the
+//terminator character, and return everything in the string AFTER the character c
+//is found.
 char	*ft_strchr(char *str, int c)
 {
 	char	ch;
