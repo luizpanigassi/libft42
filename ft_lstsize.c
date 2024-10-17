948c6edecd08bed6e031c2d6db4dca6dbbed812c
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 18:01:08 -0300

    Update ft_lstsize.c

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -9,7 +9,10 @@
 /*   Updated: 2024/10/17 15:03:36 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//This function returns the size of the node list, by iterating through the nodes
+//and checking when the next one is NULL, which indicates the node list has ended.
+//It then will increment the count and set the node as the next in line using lst->next,
+//which will continue the iteration until NULL.
 #include <stddef.h>
 
 typedef struct s_list
