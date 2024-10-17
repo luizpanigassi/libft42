a4021e5e9e8529bd44d5c7a409c214c804a1ecab
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 16:11:18 -0300

    Update ft_iascii.c

--- ft_iascii.c
+++ ft_iascii.c
@@ -9,7 +9,10 @@
 /*   Updated: 2024/10/09 18:35:52 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This simple function is used to check if the characters in a string belong in the ASCII table.
+// We use a while loop to iterate through the string and check the characters individually. 
+//If there is a single character that does not belong in the ASCII table, it returns 0, and stops the iteration.
+//To do so, we use the value of the character in the ASCII table to check it.
 int	ft_isascii(char *str)
 {
 	if (*str == '\0')
