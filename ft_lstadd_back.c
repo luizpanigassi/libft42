db3c1b6fb2c8dae326643123e7126589fd6728c6
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 18:04:14 -0300

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -9,7 +9,12 @@
 /*   Updated: 2024/10/17 15:12:32 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This function adds a new node to the end of the linked list. 
+// If the list is empty (i.e., *lst is NULL), it sets the head of 
+// the list to the new node. If the list is not empty, it iterates 
+// through the nodes until it finds the last node (where next is NULL) 
+// and updates that node's next pointer to point to the new node, 
+// effectively adding it to the end of the list.
 #include <stddef.h>
 
 typedef struct s_list
