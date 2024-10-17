85737a324d29e6f853d8a7eb9dcfa5564924407d
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 16:28:08 -0300

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -9,7 +9,10 @@
 /*   Updated: 2024/10/10 15:49:00 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//This function will fill an n set of bytes with the int c. It will then return
+//the string that contains the int c an n number of times. So, for example, if I
+//run it (str, 'z', 4), it will four 'z', followed by the string with the first four
+//characters substituted by 'z'.
 void	ft_memset(void *str, int c, int n)
 {
 	unsigned char	*ptr;
