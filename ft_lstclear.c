878a6103363280710c28de7cbf3860c57ce697ce
luizpanigassi <144566687+luizpanigassi@users.noreply.github.com>
2024-10-17 18:06:29 -0300

    Update ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -9,7 +9,12 @@
 /*   Updated: 2024/10/17 15:33:49 by luinasci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+// This function deletes all nodes in the linked list starting from the head node.
+// It takes a pointer to the head pointer (lst) and a function pointer (del) 
+// used to free the memory of each node's content. The function iterates through 
+// the list, calling the delete function on each node's content and freeing 
+// the node itself. After all nodes are deleted, it sets the head pointer 
+// to NULL to indicate that the list is empty.
 #include <stddef.h>
 
 typedef struct s_list
